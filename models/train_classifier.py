87eb37c2339a6637361b08f8c39246a8ed2442ab
John Alford <jalford@Johns-MacBook-Pro.local>
2020-01-15 13:45:02 -0500

    train_classifer: Updated GridSearch params. Also updated app_img

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -118,8 +118,8 @@ def build_model():
                         ('clf' , MultiOutputClassifier(RandomForestClassifier()))
                         ])
 
-    parameters = {'clf__estimator__n_estimators':[10,50],
-                  'clf__estimator__min_samples_split':[4]
+    parameters = {'clf__estimator__n_estimators':[10,50,75],
+                  'clf__estimator__min_samples_split':[4,8,12]
                  }
         
     model = GridSearchCV(pipeline, param_grid = parameters,cv=3)  
