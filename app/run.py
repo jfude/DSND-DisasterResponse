26f07470f44d3bbe0044622b1a2aef612ccebb1a
John Alford <jalford@Johns-MacBook-Pro.local>
2020-01-19 02:51:03 -0500

    Added two new visuals to the app, new imgs for the README.md

--- app/run.py
+++ app/run.py
@@ -1,5 +1,6 @@
 import json
 import plotly
+import numpy as np
 import pandas as pd
 import re
 import nltk
@@ -91,6 +92,13 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    ## Data for second visual
+    ## Histogram of categories represented in the DisasterDatabase.db
+    s = np.mean(df.iloc[:, 4:]).sort_values(ascending = False)
+    category_percentage     = list(s) ## percent of category in table
+    category_names          = list(s.keys())
+               
+    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -111,6 +119,31 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+
+        {
+         
+            'data': [
+                Bar(
+                    x=category_names,
+                    y=category_percentage
+                )
+            ],
+
+            'layout': {
+                'title': 'Percentage of Message Categories in Database Represented',
+                'yaxis': {
+                    'title': "Percentage",
+                    'tickformat':".1%"
+                },
+                'xaxis': {
+                    'title': "Category",
+                    'automargin': True
+                },
+                
+            }
+
+
         }
     ]
     
@@ -128,15 +161,70 @@ def go():
     # save user input in query
     query = request.args.get('query', '') 
 
+    
     # use model to predict classification for query
-    classification_labels = model.predict([query])[0]
+    classification_labels  = model.predict([query])[0]    
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
+    classification_probs   = model.predict_proba([query])
+    
+    classification_probs_results = {}
+    
+    
+    for i in range(0,len(classification_labels)):
+        if (classification_labels[i]==1.):
+            classification_probs_results.update({df.columns[4+i]:classification_probs[i][0][1]})
+    
+    
+    
+    ## Sort dictionary 'classification_probs_results' by values
+    classification_probs_results = {k: v for k, v in sorted(classification_probs_results.items(), key=lambda item: item[1])}
+    
+    classification_names = list(classification_probs_results.keys())
+    classification_probs = list(classification_probs_results.values())
+   
+    
+    ## Build plot of probabilities for categories that were found to be significant
+    ## We plot the probability of getting 1.0 for the category (as opposed to 0.0)
+    ## for significant categories. These are conventionally greater than about 60%
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=classification_probs,
+                    y=classification_names,
+                    orientation='h'
+                )
+            ],
+
+            'layout': {
+                'title': 'Classification Probabilities',
+                'yaxis': {
+                    'title': "Category"
+                },
+                'xaxis': {
+                    'title': "Probability",
+                    'tickformat':".1%",
+                    'automargin': True
+                }
+                
+            }
+        }
+    ]
+
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    
     # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
         query=query,
-        classification_result=classification_results
+        classification_result=classification_results,
+        ids = ids,
+        graphJSON = graphJSON
     )
 
 
